5179a2e64125762c4362b52b6a2b325f01668723
Ricardo Signes <rjbs@cpan.org>
2009-11-10 19:05:16 -0500

    first post

---
+++ lib/PAUSE/Client.pm
@@ -0,0 +1,28 @@
+package PAUSE::Client;
+use Moose;
+
+use namespace::autoclean;
+
+use WWW::Mechanize;
+use WWW::Mechanize::TreeBuilder;
+
+has [ qw(password username) ] => (
+  is  => 'ro',
+  isa => 'Str',
+  required => 1,
+);
+
+has mech => (
+  is   => 'ro',
+  lazy => 1,
+  default => sub {
+    my ($self) = @_;
+    my $mech = WWW::Mechanize->new;
+    WWW::Mechanize::TreeBuilder->meta->apply($mech);
+    $mech->credentials($self->username, $self->password);
+    return $mech;
+  },
+);
+
+
+1;
